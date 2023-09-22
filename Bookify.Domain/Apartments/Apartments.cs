de6b356151692e5ef2589cc4df41c5d96e65e522
Adedayo Ashiru <adedayo.ashiru@zenithbank.com>
2023-09-22 21:54:36 +0100

    Domain layer added

---
+++ Bookify.Domain/Apartments/Apartments.cs
@@ -0,0 +1,44 @@
+﻿using Bookify.Domain.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Domain.Apartments
+{
+    public sealed class Apartments : Entity
+    {
+        public Apartments(
+            Guid id,
+            Name name,
+            Description description,
+            Address address, 
+            Money price,
+            Money cleaningFee,
+            List<Amenity> amenities
+            ) : base(id) 
+        {
+            Name = name;
+            Description = description;
+            Address = address;
+            Price = price;
+            CleaningFee = cleaningFee;
+            Amenities = amenities;
+        }
+        public Guid Id { get; private set; }
+        public Name Name { get; private set; }
+        public Description  Description { get; private set; }
+        public Address Address { get; private set; }
+        public Money Price { get; }
+        public Money CleaningFee { get; }
+        public Money PriceAmount { get; private set; }
+        public Money PriceCurrency { get; private set; }
+        public Money CleaningFeeAmount { get; private set; }
+        public DateTime? LastBooked { get; private set; }
+        public List<Amenity> Amenities { get; private set; } = new();
+
+
+    }
+}
+  
