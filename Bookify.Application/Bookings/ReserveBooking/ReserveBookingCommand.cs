e1b67953c2d539b6c4ac7b218d05b79cba322b77
Adedayo Ashiru <adedayo.ashiru@zenithbank.com>
2023-10-01 22:51:43 +0100

    adding validators and logging

--- Bookify.Application/Bookings/ReserveBooking/ReserveBookingCommand.cs
+++ Bookify.Application/Bookings/ReserveBooking/ReserveBookingCommand.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using Bookify.Application.Abstractions.Messaging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Bookify.Application.Bookings.ReserveBooking
 {
-    internal class ReserveBookingCommand
-    {
-    }
+    public record ReserveBookingCommand(
+     Guid ApartmentId,
+     Guid UserId,
+     DateOnly StartDate,
+     DateOnly EndDate) : ICommand<Guid>;
 }
--- Bookify.Application/Bookings/ReserveBooking/ReserveBookingCommandHandler.cs
+++ Bookify.Application/Bookings/ReserveBooking/ReserveBookingCommandHandler.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Bookify.Application.Abstractions.Messaging;
+using Bookify.Domain.Abstractions;
+using Bookify.Domain.Apartments;
+using Bookify.Domain.Bookings;
+using Bookify.Domain.Users;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,32 @@ using System.Threading.Tasks;
 
 namespace Bookify.Application.Bookings.ReserveBooking
 {
-    internal class ReserveBookingCommandHandler
+    internal sealed class ReserveBookingCommandHandler : ICommandHandler<ReserveBookingCommand, Guid>
     {
+        private readonly IUserRepository _userRepository;
+        private readonly IApartmentRepository _apartmentRepository;
+        private readonly IBookingRepository _bookingRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly PricingService _pricingService;
+
+        public ReserveBookingCommandHandler( 
+            IUserRepository userRepository,
+            IApartmentRepository apartmentRepository, 
+            IBookingRepository bookingRepository, 
+            IUnitOfWork unitOfWork, 
+            PricingService pricingService
+            )
+        {
+            _userRepository = userRepository;
+            _apartmentRepository = apartmentRepository;
+            _bookingRepository = bookingRepository;
+            _unitOfWork = unitOfWork;
+            _pricingService = pricingService;
+        }
+
+        public async Task<Result<Guid>> Handle(ReserveBookingCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
