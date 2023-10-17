e54320f7e49f427709d39d4926d7ff5ef7cb2299
Adedayo Ashiru <adedayo.ashiru@zenithbank.com>
2023-10-17 12:55:24 +0100

    added event handler

---
+++ Bookify.Application/Bookings/GetBooking/GetBookingQueryHandler.cs
@@ -0,0 +1,18 @@
+﻿using Bookify.Application.Abstractions.Messaging;
+using Bookify.Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Application.Bookings.GetBooking
+{
+    internal sealed class GetBookingQueryHandler : IQueryHandler<GetBookingQuery, BookingResponse>
+    {
+        public Task<Result<BookingResponse>> Handle(GetBookingQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
